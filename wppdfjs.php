d7270540e02cd4f40f4203bd79a3468f31c58137
René <rene@sdo.sh>
2017-12-08 13:39:32 +0100

    refactored the plugin a bit

--- wppdfjs.php
+++ wppdfjs.php
@@ -12,8 +12,8 @@ License: GPLv2
 
 //==== Shortcode ====
 
-//tell wordpress to register the pdfjs-viewer shortcode
-add_shortcode("pdfjs-viewer", function ($incoming_from_post) {
+//tell wordpress to register the pdf-embed shortcode
+add_shortcode("pdf-embed", function ($incoming_from_post) {
     //set defaults
     $incoming_from_post = shortcode_atts([
         'url'           => 'bad-url.pdf',
@@ -25,38 +25,17 @@ add_shortcode("pdfjs-viewer", function ($incoming_from_post) {
         'openfile'      => 'false'
     ], $incoming_from_post);
 
-    $pdfjs_output = pdfjs_generator($incoming_from_post);
+    $viewer_base_url = plugins_url('pdfjs/web/viewer.php', __FILE__);
 
-    //send back text to replace shortcode in post
-    return $pdfjs_output;
-});
-
-function pdfjs_generator($incoming_from_handler)
-{
-    $viewer_base_url = plugins_url() . "/pdfjs-viewer-shortcode/pdfjs/web/viewer.php";
-
-    $file_name = $incoming_from_handler["url"];
-    $viewer_height = $incoming_from_handler["viewer_height"];
-    $viewer_width = $incoming_from_handler["viewer_width"];
-    $fullscreen = $incoming_from_handler["fullscreen"];
-    $download = $incoming_from_handler["download"];
-    $print = $incoming_from_handler["print"];
-    $openfile = $incoming_from_handler["openfile"];
-
-    if ($download != 'true') {
-        $download = 'false';
-    }
-
-    if ($print != 'true') {
-        $print = 'false';
-    }
-
-    if ($openfile != 'true') {
-        $openfile = 'false';
-    }
+    $print = $incoming_from_post["print"] != 'true' ? 'false' : 'true';
+    $openfile = $incoming_from_post["openfile"] != 'true' ? 'false' : 'true';
+    $download = $incoming_from_post["download"] != 'true' ? 'false' : 'true';
+    $file_name = $incoming_from_post["url"];
+    $fullscreen = $incoming_from_post["fullscreen"];
+    $viewer_width = (int)$incoming_from_post["viewer_width"];
+    $viewer_height = (int)$incoming_from_post["viewer_height"];
 
     $final_url = $viewer_base_url . "?file=" . $file_name . "&download=" . $download . "&print=" . $print . "&openfile=" . $openfile;
-
     $fullscreen_link = '';
 
     if ($fullscreen == 'true') {
@@ -65,8 +44,9 @@ function pdfjs_generator($incoming_from_handler)
 
     $iframe_code = '<iframe width="' . $viewer_width . '" height="' . $viewer_height . '" src="' . $final_url . '"></iframe> ';
 
+    //send back text to replace shortcode in post
     return $fullscreen_link . $iframe_code;
-}
+});
 
 //==== Media Button ====
 
@@ -77,5 +57,5 @@ add_action('media_buttons', function () {
 
 
 add_action('wp_enqueue_media', function () {
-    wp_enqueue_script('media_button', plugins_url() . '/pdfjs-viewer-shortcode/pdfjs-media-button.js', ['jquery'], '1.0', true);
+    wp_enqueue_script('media_button', plugins_url('pdfjs-media-button.js', __FILE__), ['jquery'], '1.0', true);
 });
